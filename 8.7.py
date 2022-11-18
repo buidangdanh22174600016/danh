6d47602b324f7734336359b4a94206d8469d3288
khoa hoc du lieu 16A1 <buidanh289@gmail.com>
2022-11-18 13:05:10 +0700

    updtae

---
+++ 8.7.py
@@ -0,0 +1,6 @@
+# in tổng s = 1+2+3+4+5
+sum=0
+numbers={1,2,3,4,5}
+for number in numbers:
+    sum+=number
+    print("sum =",sum)
---
+++ vd 8,5.py
@@ -0,0 +1,5 @@
+print("chương trình bảng cửu chuong 3")
+i=1
+while i<=10:
+    print("3 x", i,"=",3*i)
+    i+=1
